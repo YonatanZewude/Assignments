efdf5bc347a3d60bf1c93513ab242be180e7905b
YonatanZewude <yonatanzewude@hotmail.com>
2022-11-27 00:20:38 +0100

    implement all the basic requirements

--- ToDoList/src/JS/main.js
+++ ToDoList/src/JS/main.js
@@ -1,31 +1,87 @@
-let list = document.getElementById("theUl");
-list.addEventListener("click", strickeTrhoughElemet);
+//Fill static tasks to the todo list
+function pupulateStaticValues() {
+  const btnText = "DELETE";
+  let ul_container = document.getElementById("theUl");
+  let tasks = [
+    "Skjutsa barnen",
+    "Skola",
+    "Handla",
+    "Hämta barnen",
+    "Plugga",
+    "Sova",
+  ];
+  for (let i = 0; i < tasks.length; i++) {
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.id = "remove-btn-" + i;
+    btn.className = "remove-btn";
+    btn.innerHTML = btnText;
+    li.innerHTML = tasks[i];
+    li.appendChild(btn);
+    ul_container.appendChild(li);
+  }
+}
+pupulateStaticValues();
 
-function strickeTrhoughElemet(click_event) {
+//change tasks when done
+let list = document.getElementById("theUl");
+list.addEventListener("click", changeTasksToDone);
+
+function changeTasksToDone(click_event) {
   if (click_event.target.tagName === "LI") {
-    //click_event.target.style.visibility = 'hidden';
-    click_event.target.classList.toggle("checked");
+    let listOflis = document.getElementById("theUl").getElementsByTagName("li");
+    for (let i = 0; i < listOflis.length; i++) {
+      if (click_event.target.innerText === listOflis[i].innerText) {
+        click_event.target.style.background = "#2a9d8f";
+        let li = listOflis[i];
+        let btn = document.createElement("button");
+        btn.id = "remove-btn-" + i;
+        btn.className = "remove-btn";
+        btn.innerHTML = "DELETE";
+        li.innerHTML = "DONE";
+        li.appendChild(btn);
+        break;
+      }
+    }
   }
 }
 
-document.getElementById("btn").addEventListener("click", addNewElement);
-
-function addNewElement() {
-  let li = document.createElement("li");
-  let inputText = document.getElementById("newTask" ).value;
-  if (inputText == "") {
+//Add new tasks to the todo list
+document.getElementById("btn").addEventListener("click", function () {
+  let inputText = document.getElementById("newTask").value;
+  if (inputText.trim() == "") {
     alert("Oopps TODO task should be provided!");
   } else {
+    let btn = document.createElement("button");
+    btn.id =
+      "remove-btn-" +
+      document.getElementById("theUl").getElementsByTagName("li").length;
+    btn.className = "remove-btn";
+    btn.innerHTML = "DELETE";
+
+    let li = document.createElement("li");
     li.innerHTML = inputText;
+
+    document.getElementById("newTask").value = "";
+    li.appendChild(btn);
     document.getElementById("theUl").appendChild(li);
-    document.getElementById("newTask" ).value = "";
   }
-}
+});
 
+let ulElements = document.getElementById("theUl");
 
-document.getElementById("deletbtn").addEventListener("click", removeElement);
-function removeElement(theUl) {
-	// Removes an element from the document.
-	 let element = document. getElementById(theUl);
-	 element. parentNode. removeChild(element);
-}
+ulElements.addEventListener("click", function (evt) {
+  let listOfButtons = document
+    .getElementById("theUl")
+    .getElementsByTagName("button");
+  let listOfLis = document.getElementById("theUl").getElementsByTagName("li");
+
+  for (let i = 0; i < listOfButtons.length; i++) {
+    console.log(evt.target.id);
+    if (evt.target.id === listOfButtons[i].id) {
+      listOfLis[i].style.display = "none";
+      evt.target.style.display = "none";
+      break;
+    }
+  }
+});
